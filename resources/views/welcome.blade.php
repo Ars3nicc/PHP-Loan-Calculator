fa4ea4e0410f45240466ceb589da2cf35028ef74
Ars3nicc <franzmanrique21@gmail.com>
2022-09-10 22:16:07 +0800

    chore: routing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,21 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="{{ url('css/main.css') }}">
-    <title>Activity 1</title>
+    <title>Loan Calculator</title>
 </head>
 <body>
-    <form method="get">
-        @csrf
-        <div class="header">Loan Calculator</div>
-        <hr/>
-        <table>
-            <tr>
-                <td><label for="">Loan Amount:</label></td>
-                <td><input type="text" name="loan"></td>
-
-
-                <td>
-                Payment Terms :
+    <div class="form-container">
+        <form method="get">
+            @csrf
+            <div class="header">Loan Calculator</div>
+            <h6>Made by: Franz Manrique</h6>
+            <hr/>
+            <div class="fields">
+                <div class="input-fields">
+                    <label>Loan Amount:</label>
+                    <input class="txt-input" type="text" name="loan">
+                </div>
+                <div>
+                    <label>Payment Terms :</label>
                     <select>
                         <option value="12mon">12 Months</option>}
                         <option value="18mon">18 Months</option>
@@ -28,37 +29,38 @@
                         <option value="48mon">48 Months</option>
                         <option value="60mon">60 Months</option>
                     </select>
-                </td>
-            </tr>
-            <tr>
-                <td>
+                </div>
+            </div>
 
-                </td>
-            </tr>
-        </table>
+            <div class="button-set">
+                <input class="btn" type="submit" value="Compute">
+                <input class="btn" type="submit" value="Clear">
+            </div>
 
-
-
-        <div class="button-set">
-            <input class="btn" type="submit" value="Compose">
-            <input class="btn" type="submit" value="Clear">
+        </form>
+        <div class= "output-field">
+            <table>
+                <tr>
+                    <td><h4>Your computed results are:</h4></td>
+                </tr>
+                <tr>
+                    <td><div class="result-holder">Loan Amount: </div></td>
+                    <td><div class= "data">P{{$loan}}</div></td>
+                </tr>
+                <tr>
+                    <td><div class="result-holder">Interest: </div></td>
+                    <td><div class= "data">P{{$loan}}</div></td>
+                </tr>
+                <tr>
+                    <td><div class="result-holder">Monthly Amortization: </div></td>
+                    <td><div class= "data">P{{$loan}}</div></td>
+                </tr>
+            </table>
         </div>
-        <table>
-        <tr>
-            <th><h4>Your computed results are:</h4></th>
-        </tr>
-        <tr>
-            <td><div class="result-holder">Loan Amount: </div></td>
-        </tr>
-        <tr>
-            <td><div class="result-holder">Interest: </div></td>
-        </tr>
-        <tr>
-            <td><div class="result-holder">Monthly Amortization: </div></td>
-        </tr>
-    </table>
 
-    </form>
+    </div>
+
+
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+
+
+Route::get('/', function (Request $request) {
+
+    $loan = $request->input('loan');
+    return view('welcome', ['loan' => $loan]);
+
 });
